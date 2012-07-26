c5b27ba765e3ef4f51877fb6fa700ced532cdd80
ASAP <asap@briteideas.com>
2012-07-25 21:32:39 -0400

    Modify validation to use jQuery practices

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
-                $("input[name$='op']").click(function(){
-                var radio = $(this).val();
-                
-                if (radio == 'sqrt'){
-                    $("#snumber").hide();
-                }
-                else{
-                    $("#snumber").show();
-                }                
-            });
+    $("input[name$='op']").click(function(){
+        var radio = $(this).val();
 
-
-                $("input[name$='submit']").click(function(){
-                if(!$("input[name$='op']:checked").val() && $('input:text').is(":empty"))
-                    alert('You must select an operation and fill in text fields.');
-                else if(!$("input[name$='op']:checked").val())
-                    alert('You must select an operation.');
-                else if ($('input:text').is(":empty"))
-                    alert('Text field(s) are empty');
-            });
+        if (radio == 'sqrt') 
+            $('input#second_number').slideUp();
+        else 
+            $('input#second_number').slideDown();             
     });
+
+    $('input#calculate').click(function(){
+        var $first_number = $('input#first_number');
+        var $second_number = $('input#second_number');
+        var $operand = $('input[name$="op"]:checked');
+
+        if(!$first_number.val() && !$second_number.val() && !$operand.val()){
+            event.preventDefault();
+            alert("I don't know what to do. Click something and type stuff.");
+        }
+
+        if(!$first_number.val() || !$second_number.val()){
+            event.preventDefault();
+            alert("I can't calculate if you don't enter anything");
+        }
+
+        if(!$operand){
+            event.preventDefault();
+            alert("I don't know what I should be doing. Select an operand.");
+        }
+    });
+
+});
         
